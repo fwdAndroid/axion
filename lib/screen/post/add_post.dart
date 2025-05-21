b9fab3787831edd3241c3c52d70579ea27561f59
fwdAndroid <fwdkaleem@gmail.com>
2025-05-21 21:15:36 +0500

    FawaD

--- lib/screen/post/add_post.dart
+++ lib/screen/post/add_post.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:axion/screen/main/main_dashboard.dart';
 import 'package:axion/utils/colors.dart';
@@ -7,9 +8,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({super.key});
@@ -21,155 +26,279 @@ class AddPost extends StatefulWidget {
 class _AddPostState extends State<AddPost> {
   TextEditingController serviceNameController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  VideoPlayerController? _videoController;
+  ChewieController? _chewieController;
+
   Uint8List? _image;
+  File? _videoFile;
+  String _mediaType = '';
   bool isLoading = false;
   var uuid = Uuid().v4();
+
+  @override
+  void dispose() {
+    _videoController?.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeVideoPlayer(File videoFile) async {
+    _videoController?.dispose();
+    _chewieController?.dispose();
+
+    _videoController = VideoPlayerController.file(videoFile);
+    await _videoController!.initialize();
+
+    _chewieController = ChewieController(
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(
+            'Unsupported video format',
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+      deviceOrientationsAfterFullScreen: [DeviceOrientation.portraitUp],
+      videoPlayerController: _videoController!,
+      autoPlay: false,
+      looping: false,
+      showControls: true,
+      aspectRatio: _videoController!.value.aspectRatio,
+    );
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text("Add Feed")),
-      body: Column(
-        children: [
-          GestureDetector(
-            onTap: () => selectImage(),
-            child: Stack(
-              children: [
-                CircleAvatar(
-                  radius: 59,
-                  backgroundImage:
-                      _image != null
-                          ? MemoryImage(_image!)
-                          : const AssetImage('assets/logo.png')
-                              as ImageProvider,
-                ),
-                Positioned(
-                  bottom: -10,
-                  left: 70,
-                  child: IconButton(
-                    onPressed: () => selectImage(),
-                    icon: const Icon(Icons.add_a_photo, color: Colors.black),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(8),
+              child:
+                  _mediaType.isEmpty
+                      ? DottedBorder(
+                        color: Colors.grey,
+                        strokeWidth: 1,
+                        borderType: BorderType.RRect,
+                        radius: const Radius.circular(9),
+                        dashPattern: const [6, 3],
+                        child: Center(
+                          child: TextButton(
+                            onPressed: selectMedia,
+                            child: const Text("Add Image/Video"),
+                          ),
+                        ),
+                      )
+                      : Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          if (_mediaType == 'image') Image.memory(_image!),
+                          if (_mediaType == 'video')
+                            _chewieController != null &&
+                                    _chewieController!
+                                        .videoPlayerController
+                                        .value
+                                        .isInitialized
+                                ? Chewie(controller: _chewieController!)
+                                : const CircularProgressIndicator(),
+                          Positioned(
+                            right: 10,
+                            top: 10,
+                            child: IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: selectMedia,
+                            ),
+                          ),
+                          if (_mediaType == 'video' &&
+                              (_chewieController == null ||
+                                  !_chewieController!.isPlaying))
+                            const Icon(
+                              Icons.play_circle_filled,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                        ],
+                      ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
+              child: TextFormField(
+                controller: serviceNameController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Post Title',
+                  hintStyle: GoogleFonts.poppins(
+                    color: textColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  contentPadding: const EdgeInsets.only(left: 8, top: 15),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(4)),
+                    borderSide: BorderSide(color: mainColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: mainColor),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: mainColor),
                   ),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
-            child: TextFormField(
-              controller: serviceNameController,
-              decoration: InputDecoration(
-                hintText: 'Enter Post Title',
-                hintStyle: GoogleFonts.poppins(
-                  color: textColor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-                contentPadding: const EdgeInsets.only(left: 8, top: 15),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                  borderSide: BorderSide(color: mainColor),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: mainColor),
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: mainColor),
-                ),
-                fillColor: textColor,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: descriptionController,
-              maxLines: 4,
-              decoration: InputDecoration(
-                hintText: 'Enter Post Description',
-                hintStyle: GoogleFonts.poppins(
-                  color: textColor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: descriptionController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  hintText: 'Enter Post Description',
+                  hintStyle: GoogleFonts.poppins(
+                    color: textColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  contentPadding: const EdgeInsets.only(left: 8, top: 15),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(4)),
+                    borderSide: BorderSide(color: mainColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: mainColor),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: mainColor),
+                  ),
                 ),
-                contentPadding: const EdgeInsets.only(left: 8, top: 15),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                  borderSide: BorderSide(color: mainColor),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: mainColor),
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: mainColor),
-                ),
-                fillColor: textColor,
               ),
             ),
-          ),
-          const Spacer(),
-          isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    setState(() {
-                      isLoading = true;
-                    });
+            // const Spacer(),
+            isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (serviceNameController.text.isEmpty ||
+                          descriptionController.text.isEmpty) {
+                        showMessageBar("Please fill all fields", context);
+                        return;
+                      }
+                      if (_mediaType.isEmpty) {
+                        showMessageBar("Please select media", context);
+                        return;
+                      }
 
-                    String? imageUrl;
-                    if (_image != null) {
-                      imageUrl = await uploadImageToFirebase(_image!);
-                    }
+                      setState(() => isLoading = true);
 
-                    await FirebaseFirestore.instance
-                        .collection('feeds')
-                        .doc(uuid)
-                        .set({
-                          'titleName': serviceNameController.text,
-                          'description': descriptionController.text,
-                          'image': imageUrl, // Save only if available
-                          'date': DateTime.now(),
-                          'uuid': uuid,
-                          'uid': FirebaseAuth.instance.currentUser!.uid,
-                          'favorite': [],
-                          'comment': [],
-                        });
+                      String? mediaUrl;
+                      try {
+                        if (_mediaType == 'image') {
+                          mediaUrl = await uploadImageToFirebase(_image!);
+                        } else {
+                          mediaUrl = await uploadVideoToFirebase(_videoFile!);
+                        }
 
-                    setState(() {
-                      isLoading = false;
-                    });
+                        await FirebaseFirestore.instance
+                            .collection('feeds')
+                            .doc(uuid)
+                            .set({
+                              'titleName': serviceNameController.text,
+                              'description': descriptionController.text,
+                              'mediaUrl': mediaUrl,
+                              'mediaType': _mediaType,
+                              'date': DateTime.now(),
+                              'uuid': uuid,
+                              'uid': FirebaseAuth.instance.currentUser!.uid,
+                              'favorite': [],
+                              'comment': [],
+                            });
 
-                    showMessageBar("Feed Posted", context);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (builder) => MainDashboard()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                        showMessageBar("Feed Posted", context);
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (builder) => const MainDashboard(),
+                          ),
+                        );
+                      } catch (e) {
+                        showMessageBar("Error: $e", context);
+                      } finally {
+                        setState(() => isLoading = false);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      backgroundColor: mainColor,
+                      fixedSize: const Size(320, 60),
                     ),
-                    backgroundColor: mainColor,
-                    fixedSize: const Size(320, 60),
-                  ),
-                  child: const Text(
-                    "Add Feed",
-                    style: TextStyle(color: Colors.white),
+                    child: const Text(
+                      "Add Feed",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  Future<void> selectImage() async {
-    Uint8List ui = await pickImage(ImageSource.gallery);
-    setState(() {
-      _image = ui;
-    });
+  Future<void> selectMedia() async {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Image'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  Uint8List? image = await pickImage(ImageSource.gallery);
+                  if (image != null) {
+                    setState(() {
+                      _image = image;
+                      _mediaType = 'image';
+                      _videoFile = null;
+                      _chewieController?.dispose();
+                      _videoController?.dispose();
+                    });
+                  }
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.video_library),
+                title: const Text('Video'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  final videoXFile = await ImagePicker().pickVideo(
+                    source: ImageSource.gallery,
+                  );
+                  if (videoXFile != null) {
+                    final videoFile = File(videoXFile.path);
+                    await _initializeVideoPlayer(videoFile);
+                    setState(() {
+                      _mediaType = 'video';
+                      _videoFile = videoFile;
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Future<String> uploadImageToFirebase(Uint8List file) async {
@@ -177,6 +306,15 @@ class _AddPostState extends State<AddPost> {
       'feed_images/${Uuid().v4()}',
     );
     UploadTask uploadTask = ref.putData(file);
+    TaskSnapshot snap = await uploadTask;
+    return await snap.ref.getDownloadURL();
+  }
+
+  Future<String> uploadVideoToFirebase(File videoFile) async {
+    Reference ref = FirebaseStorage.instance.ref().child(
+      'feed_videos/${Uuid().v4()}',
+    );
+    UploadTask uploadTask = ref.putFile(videoFile);
     TaskSnapshot snap = await uploadTask;
     return await snap.ref.getDownloadURL();
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import cloud_firestore
 import emoji_picker_flutter
+import file_picker
 import file_selector_macos
 import firebase_auth
 import firebase_core
@@ -14,13 +15,17 @@ import firebase_storage
 import flutter_image_compress_macos
 import flutter_inappwebview_macos
 import google_sign_in_ios
+import package_info_plus
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
+import video_player_avfoundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
@@ -28,7 +33,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
